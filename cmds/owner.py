ab0657f8217570eaf00c532d75b0301496bf247b
L.H <michael123303@yahoo.com>
2020-11-10 11:44:52 +0800

    Add Owner program description

--- cmds/owner.py
+++ cmds/owner.py
@@ -33,17 +33,20 @@ class Owner(Cog_Extension):
     @commands.command()
     @commands.is_owner()
     async def send(self, ctx, *, msg:str):
+        '''Send the text of the command'''
         await ctx.send('%s' %(msg))
     
     @commands.command()
     @commands.guild_only()
     @commands.is_owner()
     async def del_msg(self, ctx, num:int):
+        '''Delete the number of chat records in the server'''
         await ctx.channel.purge(limit=num+1)
 
     @commands.command()
     @commands.is_owner()
     async def Aria_off(self, ctx):
+        '''Terminate program execution'''
         embed = discord.Embed(description="即將終止程式", color=0xfe5901, timestamp=datetime.datetime.utcnow())
         embed.set_footer(text="Aria Helper")
         await ctx.send(embed = embed)
